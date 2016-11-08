1533a95b69530a3fbac807179e36f96b95c4a0b2
Jeppz <jesper.hasteen@gmail.com>
2016-11-08 22:35:55 +0100

    Added navbar and login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import { LoginComponent } from './components/login/login.component';
+import {Ng2BootstrapModule} from "ng2-bootstrap";
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(routes, { useHash: true }),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2BootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
